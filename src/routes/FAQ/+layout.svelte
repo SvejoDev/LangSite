abc57467ec0f612e1c69c52083f0a4cc170069b3
Johan <johan.svensson@gtsmarketing.se>
2024-07-14 17:31:07 +0200

    testing language on route change

--- src/routes/FAQ/+layout.svelte
+++ src/routes/FAQ/+layout.svelte
@@ -3,12 +3,8 @@
   import Header from "$lib/components/Header.svelte";
   import { ParaglideJS } from "@inlang/paraglide-sveltekit";
   import { i18n } from "$lib/i18n.js";
-  import "../app.css";
+  import "../../app.css";
   import "tailwindcss/tailwind.css";
 </script>
 
-<ParaglideJS {i18n}>
-  <Header />
-
-  <slot />
-</ParaglideJS>
+<h1>Testar på de olika språken</h1>
