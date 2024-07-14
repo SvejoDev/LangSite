1cbc9321eee582aa50d8a7f2d5d237b9c6a8092f
Johan <johan.svensson@gtsmarketing.se>
2024-07-14 19:13:20 +0200

    added first product

--- src/lib/components/ProductSection.svelte
+++ src/lib/components/ProductSection.svelte
@@ -1,15 +1,13 @@
 <script>
-    import * as m from "$lib/paraglide/messages";
-</script>
+  import * as m from "$lib/paraglide/messages";
 
-const products = [
+  const products = [
     {
-        imageURL: '/excitingpaddle.png',
-        title: '{m.empty_fresh_frog_inspire()}',
-        description:'{m.empty_big_lionfish_flop()}',
-        price: '{m.soft_wide_quail_borrow()}'
+      imageURL: "/excitingpaddle.png",
+      title: m.empty_fresh_frog_inspire(), // Anropa funktionen och tilldela resultatet
+      description: m.empty_big_lionfish_flop(), // Anropa funktionen och tilldela resultatet
+      price: m.soft_wide_quail_borrow(), // Anropa funktionen och tilldela resultatet
     },
-    {
-        
-    }
-]
+    // Lägg till fler produkter om du vill
+  ];
+</script>
