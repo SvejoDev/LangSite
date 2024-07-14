b24c4faaa670a922ec61b10d6c4f8c8e8bb613ac
Johan <johan.svensson@gtsmarketing.se>
2024-07-14 18:03:30 +0200

    hero done

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let imageURL;
+  export let title;
+  export let description;
+  export let price;
+</script>
+
+<div class="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img src={imageURL} alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">{title}</h2>
+    <p>{description}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">{price}</button>
+    </div>
+  </div>
+</div>
