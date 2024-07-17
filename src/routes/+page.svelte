bc9068e21b3addfedc591e6e4dc5059c341edd09
Johan <johan.svensson@gtsmarketing.se>
2024-07-17 18:29:19 +0200

    favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,21 +13,21 @@
 
   <div class="reviews-container">
     <Review
-      quote=m.stock_best_nuthatch_dig()
+      quote={m.stock_best_nuthatch_dig()}
       image="https://lh3.googleusercontent.com/a-/ALV-UjV5GeGLMig_dSamGL0DIwIkQlNyAd2h9qaHdh9Kb2A8uOgiWwcM_w=s64-c-rp-mo-ba4-br100"
       name="Karsten Lyby  "
       title="Local guide"
     />
 
     <Review
-      quote=m.that_many_lobster_clap()
+      quote={m.that_many_lobster_clap()}
       image="https://lh3.googleusercontent.com/a-/ALV-UjUAYk6tCxzvYEd0hlnZpR2V-4Kz8Wj4hlAj9EkoC0wduYUEvffW=s64-c-rp-mo-br100"
       name="Ulf Örnhag"
       title=""
     />
 
     <Review
-      quote=m.equal_silly_hornet_trim()
+      quote={m.equal_silly_hornet_trim()}
       image="https://lh3.googleusercontent.com/a-/ALV-UjXYEuFkwDr3gWB6Eoy8U_ctSLqil9zzFVuJURqi7tri6m4xUcSf=s64-c-rp-mo-ba4-br100"
       name="Hampus Edström"
       title="Local guide"
